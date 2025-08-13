fe37b4a3461d80bcc3ffcff4706b0ec7dcf63f09
misternyarko <okukuseku@gmail.com>
2025-08-13 21:40:57 +0100

    new file:   index.html new file:   script.js
    new file:   style.css

---
+++ script.js
@@ -0,0 +1,9 @@
+function openSideNav() {
+    const nav = document.querySelector("header ul");
+    nav.style.display = "block";
+}
+
+function closeSideNav() {
+    const nav = document.querySelector("header ul");
+    nav.style.display = "none";
+}
